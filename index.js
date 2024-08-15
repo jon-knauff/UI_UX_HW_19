16d58c8707743b05f359085ecaede0558623ec2e
jon-knauff <jon.knauff@comcast.net>
2024-08-14 22:13:33 -0600

    added click sound effect

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function closePopup(){
     popup.classList.remove("open-popup");
 }
 
+document.querySelectorAll('.audio-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const audioSrc = button.getAttribute('data-audio');
+        const audio = new Audio(audioSrc);
+        audio.play();
+    });
+});
